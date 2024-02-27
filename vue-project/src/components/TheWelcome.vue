2a5194996bd14da44db4b29a5d3b500a547094cf
santipu03 <santipujolsau@gmail.com>
2024-02-27 09:16:04 +0100

    test vue

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- <button v-for="number in 10" :key="number" @click="handleClick(number)">
+      {{ number }}
+    </button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick(number) {
+      console.log(`Button ${number} clicked`);
+    },
+  },
+};
+</script>
